7058de26e3112def7b01d100ef21486c9b585fa6
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-01-25 04:13:35 +0530

    Update order.js

--- js/order.js
+++ js/order.js
@@ -374,7 +374,7 @@ function sendWhatsApp(mode, orderNo, paymentId = null) {
 // ===== RAZORPAY =====
 function startPayment(customer) {
   const options = {
-    key: "rzp_live_pfVyI37GhqWTGK",
+    key: "rzp_test_8OmRCO9SiPeXWg",
     amount: finalAmount * 100,
     currency: "INR",
     name: "Imaginary Gifts",
